8ad9adf82d3da32ea5367cae2902ef2072d1e9ca
Stefani Januario <122880008+tefJanuario@users.noreply.github.com>
2024-10-06 07:47:15 -0300

    gerenciador financeiro parte 2

--- src/test/java/br/ce/sjanu/barriga/domain/UsuarioTest.java
+++ src/test/java/br/ce/sjanu/barriga/domain/UsuarioTest.java
@@ -1,19 +1,34 @@
 package br.ce.sjanu.barriga.domain;
 
+import static br.ce.sjanu.barriga.domain.builders.UsuarioBuilder.umUsuario;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import br.ce.sjanu.barriga.domain.exceptions.ValidationException;
 
 public class UsuarioTest {
 
 	@Test
 	@DisplayName("Deve criar um usuário válido")
 	public void deveCriarUsuarioValido() {
-		Usuario usuario = new Usuario(1L, "Usuário válido", "user@mail.com", "123456");
-		assertEquals(1L, usuario.getId());
-		assertEquals("Usuário válido", usuario.getNome());
-		assertEquals("user@mail.com", usuario.getEmail());
-		assertEquals("123456", usuario.getSenha());
+		Usuario usuario = umUsuario().agora();
+		Assertions.assertAll("Usuario",
+				() -> assertEquals(1L, usuario.getId()),
+				() -> assertEquals("Usuário válido", usuario.getNome()),
+				() -> assertEquals("user@mail.com", usuario.getEmail()),
+				() -> assertEquals("123456", usuario.getSenha())
+
+		);
+	}
+	
+	@Test
+	@DisplayName("Deve rejeitar usuário sem nome")
+	public void deveRejeitarUsuarioSemNome() {
+		ValidationException ex = Assertions.assertThrows(ValidationException.class, () ->
+			umUsuario().comNome(null).agora());
+		assertEquals("Nome é obrigatório", ex.getMessage());
 	}
 }
---
+++ src/test/java/br/ce/sjanu/barriga/domain/builders/UsuarioBuilder.java
@@ -0,0 +1,48 @@
+package br.ce.sjanu.barriga.domain.builders;
+
+import br.ce.sjanu.barriga.domain.Usuario;
+
+public class UsuarioBuilder {
+	private Long id;
+	private String nome;
+	private String email;
+	private String senha;
+	private UsuarioBuilder() {}
+	
+	public static UsuarioBuilder umUsuario() {
+		UsuarioBuilder builder = new UsuarioBuilder();
+		inicializarDadosPadroes(builder);
+		return builder;
+	}
+
+	private static void inicializarDadosPadroes(UsuarioBuilder builder) {
+		builder.id = 1L;
+		builder.nome = "Usuário válido";
+		builder.email = "user@mail.com";
+		builder.senha = "123456";
+	}
+	
+	public UsuarioBuilder comId(Long param) {
+		id = param;
+		return this;
+	}
+	
+	public UsuarioBuilder comNome(String param) {
+		nome = param;
+		return this;
+	}
+	
+	public UsuarioBuilder comEmail(String param) {
+		email = param;
+		return this;
+	}
+	
+	public UsuarioBuilder comSenha(String param) {
+		senha = param;
+		return this;
+	}
+	
+	public Usuario agora() {
+		return new Usuario(id, nome, email, senha);
+	}
+}
